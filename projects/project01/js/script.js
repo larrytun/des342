a44c0e2ac17944424fd298436e83f517a63af30e
Larry Dean Tunseth <tunseth@pdx.edu>
2023-05-04 01:02:57 -0700

    Add clock to taskbar

--- projects/project01/js/script.js
+++ projects/project01/js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+  function displayCurrentTime() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    let timeString = hours + ':' + minutes + ' ' + ampm;
+    $('#current-time').text(timeString);
+  }
+    displayCurrentTime();
+    setInterval(displayCurrentTime, 3000);
   $('#power').click(function(){
     $('#power').toggleClass('on');
     $('.wrapper').toggleClass('zoom');
